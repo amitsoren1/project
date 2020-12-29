15e94aac30f87b6d0946ee1347141b7b16b390f5
Your Name <you@example.com>
2020-12-29 10:51:22 +0530

    truryuy

--- yu.py
+++ yu.py
@@ -18,7 +18,16 @@ class SetupSSL:
     clone_url_command = ['git','clone','https://github.com/acmesh-official/acme.sh']
     upgrade_acme_command = ['./acme.sh','--upgrade']
     install_acme_command = ['./acme.sh','--install']
-    # test_acme_command = ['acme.sh','-v']
+    issue_cert_command = ['./acme.sh', '--issue', '-d', 'domain.com', '--dns', 
+                            '--yes-I-know-dns-manual-mode-enough-go-ahead-please']
+    renew_cert_command = ['./acme.sh', '--renew', '-d', 'domain.com', '--dns', 
+                            '--yes-I-know-dns-manual-mode-enough-go-ahead-please']
+    test_acme_command = ['acme.sh','-v']
+    def __init__(self,domain,verify):
+        self.domain = domain
+        self.issue_cert_command[3] = self.renew_cert_command[3] = self.domain
+        self.verify =verify
+
     def run_commands(self,commands):
         process = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -37,32 +46,30 @@ class SetupSSL:
         return outputs,errors_and_warnings
     
     def install_acme(self):
-        # out,warn_err = self.run_commands(self.test_acme_command)
-        # if len(warn_err) == 0:
-        #     print("upgrading...")
-        #     out,warn_err = self.run_commands(self.upgrade_acme_command)
-        #     print(out[-1])
-        # else:
-        out,warn_err = self.run_commands(self.clone_url_command)
-        for err in warn_err:
-            print(err)
-        currdir = os.getcwd()
-        os.chdir(os.path.join(currdir,"acme.sh"))
+        # out,warn_err = self.run_commands(self.clone_url_command)
+        # for err in warn_err:
+        #     print(err)
+        # self.currdir = os.getcwd()
+        # os.chdir(os.path.join(self.currdir,"acme.sh"))
 
-        print("upgrading...")
-        out,warn_err = self.run_commands(self.upgrade_acme_command)
-        print(out[-1])
+        # print("upgrading...")
+        # out,warn_err = self.run_commands(self.upgrade_acme_command)
+        # print(out[-1])
 
-        out, warn_err = self.run_commands(self.install_acme_command)
-        for x in out:
-            print(x)
-        os.chdir(currdir)
+        # out, warn_err = self.run_commands(self.install_acme_command)
+        # for x in out:
+        #     print(x)
+        self.run_commands(self.test_acme_command)
+    
+    def issue_cert(self):
+        pass
 
 if __name__ == '__main__':
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument('--d', action='store', type=str)
-    my_parser.add_argument('--r', action='store', type=str)
+    my_parser.add_argument('--v', action='store', type=str)
     args = my_parser.parse_args()
     # print(vars(args))
-    obj = SetupSSL()
+    obj = SetupSSL(vars(args)['d'],vars(args)['v'])
     obj.install_acme()
+    # print(obj.issue_cert_command)
